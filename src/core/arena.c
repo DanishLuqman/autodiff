9355e10630e8b22fbc258aaf6194c902088681a7
DanishLuqman <danish@rosarylabs.ai>
2026-07-16 17:17:02 +0800

    refactor: remove redundant check

--- src/core/arena.c
+++ src/core/arena.c
@@ -1,6 +1,6 @@
 #include "ad/arena.h"
 #include <stdlib.h>
-#include <assert.h>
+#include <stdio.h>
 
 #define AD_ARENA_ALIGN 16
 #define AD_ARENA_ALIGN_UP(size) (((size) + (AD_ARENA_ALIGN - 1)) & ~((size_t)AD_ARENA_ALIGN - 1))
@@ -13,9 +13,12 @@ void arena_init(Arena *a, size_t capacity){
 }
 
 void *arena_alloc(Arena *a, size_t size){   //size_t just speaks about how the variable is stored, when passing through malloc, it is just a number, no relation to whether its 8 bit or what not
-    assert(size <= a->capacity - a->offset);
     size_t aligned = AD_ARENA_ALIGN_UP(size); 
-    assert(aligned <= (a->capacity - a->offset));
+    if (aligned > a->capacity - a->offset){
+        fprintf(stderr, "arena: out of memory (need %zu , have %zu)\n",
+        aligned, a->capacity - a->offset);
+        abort();
+    }
     void *p = a->base + a->offset;
     a->offset += aligned;
     return p;
